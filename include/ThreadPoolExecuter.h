68a176c15aa6f96a6758d8f89266444c214dc9e4
Harsh <enggharsh18@gmail.com>
2025-06-22 21:03:19 +0530

    removed error and moved defination of enque into header

--- include/ThreadPoolExecuter.h
+++ include/ThreadPoolExecuter.h
@@ -25,3 +25,34 @@ class ThreadPoolExecuter {
         template<class F, class... Args>
         auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>>;
 };
+
+/**
+ * @brief Enqueues a task into the thread pool and returns a future to retrieve the result.
+ * @tparam F Callable type.
+ * @tparam Args Argument types.
+ * @param f Callable to execute.
+ * @param args Arguments to pass to the callable.
+ * @return std::future<typename std::invoke_result_t<F, Args...>> Future to retrieve the result.
+ */
+template<class F, class... Args>
+auto ThreadPoolExecuter::enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>> {
+    using returnType = typename std::invoke_result_t<F, Args...>;
+
+    auto taskPtr = std::make_shared<std::packaged_task<returnType()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    std::future<returnType> result = taskPtr->get_future();
+
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        if (stop) {
+            throw std::runtime_error("Cannot submit tasks to a stopped ThreadPoolExecuter");
+        }
+        task.emplace([taskPtr]() { (*taskPtr)(); });
+    }
+
+    queueCondition.notify_one();
+    return result;
+}
+
--- src/ThreadPoolExecuter.cpp
+++ src/ThreadPoolExecuter.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 #include <iostream>
 #include <thread>
@@ -74,33 +75,4 @@ ThreadPoolExecuter :: ~ThreadPoolExecuter(){
     shutdown();
 }
 
-/**
- * @brief Enqueues a task into the thread pool and returns a future to retrieve the result.
- * @tparam F Callable type.
- * @tparam Args Argument types.
- * @param f Callable to execute.
- * @param args Arguments to pass to the callable.
- * @return std::future<typename std::invoke_result_t<F, Args...>> Future to retrieve the result.
- */
-template<class F, class... Args>
-auto ThreadPoolExecuter::enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>> {
-    using returnType = typename std::invoke_result_t<F, Args...>;
-
-    auto taskPtr = std::make_shared<std::packaged_task<returnType()>>(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-    );
-
-    std::future<returnType> result = taskPtr->get_future();
-
-    {
-        std::unique_lock<std::mutex> lock(queueMutex);
-        if (stop) {
-            throw std::runtime_error("Cannot submit tasks to a stopped ThreadPoolExecuter");
-        }
-        task.emplace([taskPtr]() { (*taskPtr)(); });
-    }
-
-    queueCondition.notify_one();
-    return result;
-}
 
