228898de8aff83c8a61c133cc1ca134e7da5854a
Harsh <enggharsh18@gmail.com>
2025-07-02 01:00:41 +0530

    removing resource not found handler issue

--- src/ResourceNotFoundHandler.cpp
+++ src/ResourceNotFoundHandler.cpp
@@ -1,8 +1,8 @@
-#include "../include/RequestNotFoundHandler.h"
+#include "../include/ResourceNotFoundHandler.h"
 #include "../include/HttpRequest.h"
 #include "../include/HttpResponse.h"
 
-void RequestNotFoundHandler::handle(const HttpRequest&, HttpResponse& httpResponse) {
+void ResourceNotFoundHandler::handle(const HttpRequest&, HttpResponse& httpResponse) {
     httpResponse.setStatus(404, "Not Found");
     httpResponse.setBody("<html><body>Not Found</body></html>");
 }
