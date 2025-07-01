1541f17bafa6eb77fe65fdd2391dcc202aac0d24
Harsh <enggharsh18@gmail.com>
2025-07-02 00:37:50 +0530

    Fixed name of request not found handler

--- src/RequestNotFoundHandler.cpp
+++ src/RequestNotFoundHandler.cpp
@@ -1,8 +1,8 @@
-#include "../include/ResourceNotFoundHandler.h"
+#include "../include/RequestNotFoundHandler.h"
 #include "../include/HttpRequest.h"
 #include "../include/HttpResponse.h"
 
-void ResourceNotFoundHandler::handle(const HttpRequest&, HttpResponse& httpResponse) {
+void RequestNotFoundHandler::handle(const HttpRequest&, HttpResponse& httpResponse) {
     httpResponse.setStatus(404, "Not Found");
     httpResponse.setBody("<html><body>Not Found</body></html>");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "../include/HttpServer.h"
+#include "../include/HelloWorldController.h"
 
 int main(){
     int port = 8080;
     HttpServer server(port);
-
-    server.registerHandler("GET", "/hello", new HelloWorldController());
+    std::string method = "GET";
+    server.registerHandler(method, "/hello", new HelloWorldController());
     
     server.start();
     return 0;
 }
+
